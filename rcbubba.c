6df65bc02a541efd073081cf284c4e0d1dcaa17c
ehiar <edwardhiar@gmail.com>
2013-11-29 14:07:08 -0800

    Fixed naming in comments.

--- rcbubba.c
+++ rcbubba.c
@@ -6,28 +6,28 @@
 
 task main()
 {
-  int threshold = 20;                      /* Int 'threshold' will allow us to ignore low       */
-                                           /* readings that keep our robot in perpetual motion. */
+  int threshold = 20;                        /* Int 'threshold' will allow us to ignore low       */
+                                             /* readings that keep our robot in perpetual motion. */
 
-  while(true)                              // Infinite loop:
+  while(true)                                // Infinite loop:
   {
     getJoystickSettings(joystick);
 		if(abs(joystick.joy1_y2) > threshold)   // If the right analog stick's Y-axis readings are either above or below the threshold:
     {
-      motor[motorD] = joystick.joy1_y2;     // Motor B is assigned a power level equal to the right analog stick's Y-axis reading.
+      motor[motorD] = joystick.joy1_y2;     // Motor D is assigned a power level equal to the right analog stick's Y-axis reading.
     }
     else                                    // Else if the readings are within the threshold:
     {
-      motor[motorD] = 0;                    // Motor B is stopped with a power level of 0.
+      motor[motorD] = 0;                    // Motor D is stopped with a power level of 0.
     }
 
     if(abs(joystick.joy1_y1) > threshold)   // If the left analog stick's Y-axis readings are either above or below the threshold:
     {
-      motor[motorE] = joystick.joy1_y1;     // Motor C is assigned a power level equal to the left analog stick's Y-axis reading.
+      motor[motorE] = joystick.joy1_y1;     // Motor E is assigned a power level equal to the left analog stick's Y-axis reading.
     }
     else                                    // Else if the readings are within the threshold:
     {
-      motor[motorE]= 0;                    // Motor C is stopped with a power level of 0.
+      motor[motorE]= 0;                     // Motor e is stopped with a power level of 0.
     }
   }
 }
